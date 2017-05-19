e4ec4b4dcf617ab3976b333e4fde8a0497b8a3cb
HesselTjeerdsma <Hesseltjeerdsma@gmail.com>
2017-05-19 11:18:08 +0200

    updated
    added true&false

--- DetectSample.ino
+++ DetectSample.ino
@@ -1,34 +1,31 @@
-void DetectSample()
-{ 
-  for (int Steps = 0; Steps < 12; Steps = Steps++)
-  {
-  servoHead.write(15*Steps);
-  delay(1000);
-  float UltraSoundTop = sonarTop.convert_cm(sonarTop.ping_median(5)); //pings the distance 5 times, takes the median of close values and converts it to cm
-  float UltraSoundBottom = sonarBottom.convert_cm(sonarBottom.ping_median(5)); //pings the distance 5 times, takes the median of close values and converts it to cm 
-  
-  int Sample = 0;
-  int Wall = 0;
-  
-  if (Sample == 0)
-   {
-    if (UltraSoundTop < 150 && UltraSoundBottom < 150)
-    Wall = 1;
-    else if (UltraSoundTop > 150 && UltraSoundBottom < 150)
-    Sample = 1;
+void DetectSample() { 
+  bool Sample = false;
+  bool Wall = false;
+  for (int Steps = 0; Steps < 12; Steps++) {
+    servoHead.write(15*Steps);
+    delay(1000);
+    float UltraSoundTop = sonarTop.convert_cm(sonarTop.ping_median(5)); //pings the distance 5 times, takes the median of close values and converts it to cm
+    float UltraSoundBottom = sonarBottom.convert_cm(sonarBottom.ping_median(5)); //pings the distance 5 times, takes the median of close values and converts it to cm 
+    if (Sample == false) {
+      if (UltraSoundTop < 150 && UltraSoundBottom < 150) {
+        Wall = true;
+      }
+      else if(UltraSoundTop > 150 && UltraSoundBottom < 150) {
+        Sample = true;
+      }
    }
-  if (Wall == 1)
-  {
+  if (Wall == true) {
     Object = 2;
   }
-  if (Sample == 1)
-  {
+  if (Sample == true) {
     Object = 1;
   }
-  if (Sample == 0 && Wall == 0)
-  {Object = 0;}
-  if (Object == 1)
-  {MoveToObject;
+  if (Sample == false && Wall == false) {
+      Object = 0;
+    }
+  if (Object == true) {
+    MoveToObject();
+  }
    break;
   }
   
